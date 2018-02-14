df31ed3787c6320be3ceee0e48e8af47bdd670a8
tkt <ketan.tang@dji.com>
2018-02-14 11:50:53 +0800

    bmp2pgm supports plain pgm format

--- bmp2pgm.cpp
+++ bmp2pgm.cpp
@@ -3,14 +3,19 @@
 int main(int argc, char** argv)
 {
 	if(argc<3){
-		printf("Usage: %s <image.bmp> <image.pgm>\n", argv[0]);
+		printf("Usage: %s <image.bmp> <image.pgm> [is_plain=1]\n", argv[0]);
 		return 0;
+	}
+	bool is_plain = true;
+	
+	if(argc>3){
+		is_plain = atoi(argv[3]);
 	}
 	
 	int height, width;
 	unsigned char* im=readBMPGray(argv[1], width, height);
 	
-	writePGM(argv[2], im, width, height);
+	writePGM(argv[2], im, width, height, is_plain);
 	delete[] im;
 	return 1;
 }
